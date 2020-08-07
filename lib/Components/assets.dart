beceb42e599cd92dd4060fc97ac7cd70c0a1cf84
zeeshan__07 <zeeshanbhati99@gmail.com>
2020-08-07 18:44:01 +0530

    IntroSlider Added

---
+++ lib/Components/assets.dart
@@ -0,0 +1,21 @@
+
+
+const List<String> images = [
+
+  'https://firebasestorage.googleapis.com/v0/b/my-project-1-f8563.appspot.com/o/img%2Frsz_med.png?alt=media&',
+  'https://firebasestorage.googleapis.com/v0/b/my-project-1-f8563.appspot.com/o/img%2Frsz_med.png?alt=media&',
+  'https://firebasestorage.googleapis.com/v0/b/my-project-1-f8563.appspot.com/o/img%2Frsz_med.png?alt=media&',
+  'https://firebasestorage.googleapis.com/v0/b/my-project-1-f8563.appspot.com/o/img%2Fclipboard-removebg-preview.png?alt=media&',
+  'https://firebasestorage.googleapis.com/v0/b/my-project-1-f8563.appspot.com/o/img%2Fchat-benefits.png?alt=media&',
+  'https://firebasestorage.googleapis.com/v0/b/my-project-1-f8563.appspot.com/o/img%2Frsz_med.png?alt=media&',
+  'https://firebasestorage.googleapis.com/v0/b/my-project-1-f8563.appspot.com/o/img%2Frsz_med.png?alt=media&',
+
+];
+
+//const List<String> backgroundImages = [
+//  'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2Fb1.jpg?alt=media',
+//  'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2Fb2.jpg?alt=media',
+//  'https://firebasestorage.googleapis.com/v0/b/dl-flutter-ui-challenges.appspot.com/o/img%2Fb3.jpg?alt=media',
+//];
+
+

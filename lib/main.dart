fe01aa5040e093d9b80ff1fc8828c83fe034080b
tanharpatel <trpatel2906@gmail.com>
2020-08-09 16:13:29 +0530

    merge zeeshan to dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:The_Mindfulness/Class/Tabs.dart';
+import 'package:The_Mindfulness/Screens/DoctorLogin.dart';
+import 'package:The_Mindfulness/Screens/IntroSlider.dart';
+import 'package:The_Mindfulness/Screens/LoginScreen.dart';
+import 'package:The_Mindfulness/Screens/RegistrationScreen.dart';
+import 'package:The_Mindfulness/Screens/WelcomeScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      initialRoute: '/tabs',
-      routes: {
-        '/tabs': (context) => defaultTabController(),
+      title: "The Mindfulness",
+      builder: (context,child)=> MediaQuery(
+        data:MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+        child: child,
+      ),
+      initialRoute: IntroSlider.id,
+      routes: <String,WidgetBuilder>{
+        IntroSlider.id:(BuildContext context)=> IntroSlider(),
+        WelcomeScreen.id:(BuildContext context)=> WelcomeScreen(),
+        RegistrationScreen.id:(BuildContext context)=>RegistrationScreen(),
+        LoginScreen.id:(BuildContext context)=>LoginScreen(),
+        DoctorLogin.id:(BuildContext context)=>DoctorLogin(),
       },
     )
   );
